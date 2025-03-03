935d00c78919ac805109994358ee9634687fdf99
Elenor <elenor@elenor.net>
2025-03-03 15:01:04 -0800

    Add missing packages

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 export const API_BASE_URL = `https://nametag-duckit-2.uc.r.appspot.com`;
 
-export const USE_PROXY = true;
+export const USE_PROXY = false;
 export const API_PROXY_HOST = 'localhost';
 export const API_PROXY_PORT = 3000;
 export const API_PROXY_PROTOCOL = 'http';
